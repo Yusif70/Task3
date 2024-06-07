e2f28fade00a2027114f10ef4c22f5f4c2059344
Yusif70 <yusifpirquliyev7@gmail.com>
2024-06-07 17:01:36 +0400

    initial commit

---
+++ namespaces/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace task1
+{
+	internal class Class
+	{
+		public string FindFirstAndLast(int[] nums, int target)
+		{
+			int start = -1;
+			int end = -1;
+			for(int i = 0; i <nums.Length; i++)
+			{
+				if (nums[i] == target)
+				{
+					start = i;
+					break;
+				}
+			}
+			for(int i = nums.Length - 1; i >= 0; i--)
+			{
+				if (nums[i] == target)
+				{
+					end = i;
+					break;
+				}
+			}
+
+            return start + "," + end;
+        }
+	}
+}
+
+namespace task2 {
+	class Class
+	{
+		public void computePower(int[] nums)
+		{
+            Console.WriteLine("\n val^val: ");
+            foreach (int num in nums)
+			{
+				int newNum = 1;
+				for(int i = 0; i< num; i++)
+				{
+					newNum *= num;
+				}
+                Console.Write(newNum + " ");
+            }
+			
+			Console.WriteLine("\n val^2: ");
+			foreach (int num in nums)
+			{
+				Console.Write(num*num + " ");
+			}
+
+            Console.WriteLine("\n 2^val: ");
+            foreach (int num in nums)
+			{
+				int newNum = 1;
+				for (int i = 0; i < num; i++)
+				{
+					newNum *= 2;
+				}
+				Console.Write(newNum + " ");
+			}
+        }
+	}
+
+}
+
---
+++ namespaces/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace namespaces
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = { 1, 2, 3, 3, 4, 5 };
+
+			task1.Class obj = new task1.Class();
+			task2.Class obj2 = new task2.Class();
+
+            Console.WriteLine("task1: ");
+            Console.WriteLine("ilk ve sonuncu indeks: " + obj.FindFirstAndLast(nums, 3));
+
+			
+			foreach (int num in nums)
+			{
+				Console.Write(num + " ");
+			}
+
+            obj2.computePower(nums);
+			Console.WriteLine();
+
+		}
+	}
+}
